9993faafac0bdea7dac5ea09aba83064dd1927c8
hogni89 <hognibeinisson@gmail.com>
2021-11-19 00:07:35 +0000

    docker

--- Users/Auth/app.js
+++ Users/Auth/app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Mr. Beinisson');
+    res.end('Hello Mr. Beinisson from Docker!');
 });
 
 server.listen(port, hostname, () => {
