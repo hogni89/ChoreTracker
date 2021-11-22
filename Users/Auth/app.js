7fb4d8bd492b3e24c6e4069c835d726591d5117a
root <root@pop-os.localdomain>
2021-11-22 07:29:43 +0000

    password hash

--- Users/Auth/app.js
+++ Users/Auth/app.js
@@ -3,12 +3,47 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000;
 
+const saltRounds = 10;
+const bcrypt = require('bcrypt');
+const plainText = "Hello World";
+const loginPassword = "Hello World";
+
+function hashPassword(password) { 
+    return bcrypt.hashSync(password, saltRounds);
+} 
+
+function checkPassword(loginPassword, hashedPassword) { 
+    return bcrypt.compareSync(loginPassword, hashedPassword);
+} 
+
+function main() { 
+    const hashedPassword = hashPassword(plainText, 10);
+    console.log(checkPassword(loginPassword, hashedPassword))
+}
+
+if (require.main === module) {
+    main();
+}
+
+/*
+
+bcrypt.compare(loginPassword, hashedPassword)
+    .then(result => {
+        console.log(result);
+    });
+
+  */  
+
+/*
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Mr. Beinisson from Docker!');
+    res.end('Hello Mr. Beinisson!');
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+*/
